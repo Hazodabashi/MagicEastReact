19cafdb5967acf094013bc8331b7c4eae73addd6
Fernando Garcia <fern.garciac@duocuc.cl>
2025-10-20 00:09:56 -0300

    fix: preparar server.js y Dockerfile para deploy en Railway

--- src/components/BOStock.jsx
+++ src/components/BOStock.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./BOcss.css";
 import { Link } from "react-router-dom";
 
-// Import directo del JSON
+
 import productosData from "../Data/Productos.json";
 
 function StockBackOffice() {
